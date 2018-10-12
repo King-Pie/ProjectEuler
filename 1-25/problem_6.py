d673c3e08a67eff31ba8dda8ebc05db67535ed74
Sam <sam.taylor92@hotmail.com>
2018-10-12 14:05:40 +0100

    added problem 6

---
+++ 1-25/problem_6.py
@@ -0,0 +1,19 @@
+# The sum of the squares of the first ten natural numbers is,
+# 1^2 + 2^2 + ... + 10^2 = 385
+# The square of the sum of the first ten natural numbers is,
+# (1 + 2 + ... + 10)2 = 552 = 3025
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the
+# sum is 3025 − 385 = 2640.
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+import itertools
+
+n = 100
+n_range = range(1, n+1)
+combos = (list(itertools.combinations(n_range, 2)))
+
+total = 0
+for c in combos:
+    total += 2 * c[0] * c[1]
+
+print(total)
